7e494284267305ca47197b0abd480e7990e61ac8
Omotosho Oluwatobiju <tobijudah@gmail.com>
2020-05-12 18:23:58 +0100

    added error messages to improve UX

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,35 +9,47 @@ let errorMessage = document.querySelector('#error')
 
 
 let init = () => {
-   
+   submit.addEventListener('click', () => {
+      if (bill.value > 0 && people.value > 0 && percent.value > 0 && percent.value <= 100) {
+         calcTotalTip()
+
+         calcTipPerPerson()
+
+         calcTotalAmount()
+      }
+
+      else if (percent.value > 100) {
+         showError('Percentage must be less than 100')
+      }
+
+      else {
+         showError('All values should be positive numbers only')
+      }
+   })
 }
 
 
-submit.addEventListener('click', () => {
-
-   calcTotalTip()
-
-   _tipPerPerson = ( _totalTip / parseInt(people.value)).toFixed(2)
-   tipPerPerson.textContent = `$${_tipPerPerson}`;
-
-   _totalPrice = (_totalTip + parseFloat(bill.value)).toFixed(2)
-   totalPrice.textContent = `$${_totalPrice}`;
-
-})
 
 
 
 let calcTotalTip = () => {
 
-   if (bill.value < 1){
-      showError('ADD A VALUE GREATER THAN ONE')
-   }
-   else {
-      _totalTip = parseFloat((bill.value * (percent.value / 100)).toFixed(2))
-      totalTip.textContent = `$${_totalTip}`;
-   } 
+   _totalTip = parseFloat((bill.value * (percent.value / 100)).toFixed(2))
+   totalTip.textContent = `$${_totalTip}`;
 }
 
+let calcTipPerPerson = () => {
+
+   _tipPerPerson = ( _totalTip / parseInt(people.value)).toFixed(2)
+   tipPerPerson.textContent = `$${_tipPerPerson}`;
+}
+
+let calcTotalAmount = () => {
+
+   _totalPrice = (_totalTip + parseFloat(bill.value)).toFixed(2)
+   totalPrice.textContent = `$${_totalPrice}`;
+}
+  
 let showError = (error) => {
    errorMessage.style.display = 'block'
    errorMessage.textContent = error
